a89f58c615b770be796c4562e654da9c06229a3a
deivid <crusedmoss@gmail.com>
2015-10-31 13:26:36 -0600

    problema 1 euler

--- suma_multiplos.py
+++ suma_multiplos.py
@@ -1,17 +1,13 @@
 """suma de multiplos"""
 
-multi3 = []
-multi5 = []
+multiplos = []
 
 for i in range(1000):
-	if i % 3 == 0:
-		multi3.append(i)
-	elif i % 5 == 0:
-		multi5.append(i)
+	if i % 3 == 0 or i % 5 == 0:
+		multiplos.append(i)
 
-suma1  = sum(multi3)
-suma2 = sum(multi5)
-suma3 = suma1 + suma2
-print suma3
+suma1  = sum(multiplos)
+
+print suma1
 
 
