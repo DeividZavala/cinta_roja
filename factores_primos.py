ceb033baec219b857b653c7e1fb9a7616946847c
deivid <crusedmoss@gmail.com>
2015-11-06 21:07:12 -0600

    problema euler

---
+++ factores_primos.py
@@ -0,0 +1,13 @@
+"""Factores primos"""
+
+num = input('Ingresa el numero: ')
+
+def fac_primos(num):
+	x = int(2)
+	while (num != 1):
+		if num % x = 0:
+			print(str(x) + ' ')
+			num  = num/x
+		else:
+			x = x+1
+print fac_primos(num)
